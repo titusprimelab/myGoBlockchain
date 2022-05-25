906594e7dffa5a7c4873b17a5c9e0022a36ad49f
Titus <tkivite@gmail.com>
2022-05-25 13:08:02 +0300

    added persistence with badger a key-value db

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,4 +1,9 @@
 package blockchain
+import (
+	"bytes"
+	"encoding/gob"
+	"log"
+)
 type Block struct {
 	Hash     []byte
 	Data     []byte
@@ -6,9 +11,9 @@ type Block struct {
 	Nonce    int 
 }
 
-type BlockChain struct {
-	Blocks []*Block
-}
+// type BlockChain struct {
+// 	Blocks []*Block
+// }
 
 func CreateBlock(data string, prevHash []byte) *Block {
 	block := &Block{[]byte{}, []byte(data), prevHash, 0}
@@ -21,16 +26,38 @@ func CreateBlock(data string, prevHash []byte) *Block {
 	return block
 }
 
-func (chain *BlockChain) AddBlock(data string) {
-	prevBlock := chain.Blocks[len(chain.Blocks)-1]
-	new := CreateBlock(data, prevBlock.Hash)
-	chain.Blocks = append(chain.Blocks,new)
-}
+// func (chain *BlockChain) AddBlock(data string) {
+// 	prevBlock := chain.Blocks[len(chain.Blocks)-1]
+// 	new := CreateBlock(data, prevBlock.Hash)
+// 	chain.Blocks = append(chain.Blocks,new)
+// }
 
 func Genesis() *Block {
 	return CreateBlock("Genesis", []byte{})
 }
 
-func InitBlockChain() *BlockChain {
-	return &BlockChain{[]*Block{Genesis()}}
+// func InitBlockChain() *BlockChain {
+// 	return &BlockChain{[]*Block{Genesis()}}
+// }
+
+func Handle(err error) {
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func (b *Block) Serialize() []byte {
+	var res bytes.Buffer
+	encoder := gob.NewEncoder(&res)
+	err := encoder.Encode(b)
+	Handle(err)
+	return res.Bytes()
+}
+
+func Deserialize(data []byte) *Block {
+    var block Block
+    decoder := gob.NewDecoder(bytes.NewReader(data))
+    err := decoder.Decode(&block)
+    Handle(err)
+    return &block
 }
